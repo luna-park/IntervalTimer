5a8e63ed634ccd7803a1e9847edaa770364a3df9
znak <tynuza@gmail.com>
2016-06-13 21:52:29 +0300

    Initial commit

---
+++ app/src/main/java/org/lunapark/dev/intervaltimer/MainActivity.java
@@ -0,0 +1,206 @@
+package org.lunapark.dev.intervaltimer;
+
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        SoundPool.OnLoadCompleteListener, SetTime.DialogListener {
+
+    private ProgressBar progressBar;
+    private Button btnStart, btnStop, btnTime1, btnTime2;
+    private TextView tvInterval;
+
+    private Bundle bundle;
+
+    private int timeInterval1 = 20;
+    private int timeInterval2 = 10;
+    private boolean running = false;
+
+    private SoundPool soundPool;
+    private int sndBlip1, sndBlip2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnStop.setEnabled(false);
+        btnStop.setOnClickListener(this);
+
+        btnTime1 = (Button) findViewById(R.id.btnTime1);
+        btnTime1.setOnClickListener(this);
+
+        btnTime2 = (Button) findViewById(R.id.btnTime2);
+        btnTime2.setOnClickListener(this);
+
+        updateIntervals();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+
+        tvInterval = (TextView) findViewById(R.id.tvInterval);
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        soundPool.setOnLoadCompleteListener(this);
+
+        try {
+            sndBlip1 = soundPool.load(getAssets().openFd("blip1.wav"), 1);
+            sndBlip2 = soundPool.load(getAssets().openFd("blip2.wav"), 1);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        bundle = new Bundle();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnStart:
+                running = true;
+                AsyncTimer asyncTimer = new AsyncTimer();
+                asyncTimer.execute();
+                break;
+            case R.id.btnStop:
+                running = false;
+                break;
+
+            case R.id.btnTime1:
+                setTimeDialog(timeInterval1, 1);
+                break;
+            case R.id.btnTime2:
+                setTimeDialog(timeInterval2, 2);
+                break;
+        }
+    }
+
+    private void setTimeDialog(int time, int interval) {
+        DialogFragment newFragment = new SetTime();
+        bundle.putInt("Min", Math.round(time / 60));
+        bundle.putInt("Sec", time % 60);
+        bundle.putInt("Interval", interval);
+        newFragment.setArguments(bundle);
+        newFragment.show(getSupportFragmentManager(), "SetTime");
+    }
+
+    @Override
+    protected void onDestroy() {
+        running = false;
+        soundPool.release();
+        super.onDestroy();
+    }
+
+    private String getTimeString(int input) {
+        int min = Math.round(input / 60);
+        int sec = input % 60;
+        return String.format("%02d:%02d", min, sec);
+    }
+
+    @Override
+    public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog, int seconds, int interval) {
+        if (seconds < 2) seconds = 2;
+        switch (interval) {
+            case 1:
+                timeInterval1 = seconds;
+                break;
+            case 2:
+                timeInterval2 = seconds;
+                break;
+        }
+
+        updateIntervals();
+    }
+
+    private void updateIntervals() {
+        btnTime1.setText(getTimeString(timeInterval1));
+        btnTime2.setText(getTimeString(timeInterval2));
+    }
+
+    class AsyncTimer extends AsyncTask<Void, Integer, Void> {
+
+        private Integer loop = 0;
+
+        @Override
+        protected void onPreExecute() {
+            btnStart.setEnabled(false);
+            btnTime1.setEnabled(false);
+            btnTime2.setEnabled(false);
+            btnStop.setEnabled(true);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                while (running) {
+                    loop++;
+                    soundPool.play(sndBlip2, 1, 1, 0, 1, 1);
+                    for (int i = 0; i < timeInterval1; i++) {
+                        if (!running) break;
+                        Integer finish = timeInterval1;
+                        Integer current = i;
+                        publishProgress(finish, current, loop);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+                    soundPool.play(sndBlip1, 1, 1, 0, 0, 1);
+
+                    for (int i = 0; i < timeInterval2; i++) {
+                        if (!running) break;
+                        Integer finish = timeInterval2;
+                        Integer current = i;
+                        publishProgress(finish, current, loop);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
+
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            int limit = values[0];
+            int result = values[1];
+            int progress = Math.round(100 * result / limit);
+            progressBar.setProgress(progress);
+            tvInterval.setText(values[2] + "\n" + getTimeString(limit - result));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            btnStart.setEnabled(true);
+            btnTime1.setEnabled(true);
+            btnTime2.setEnabled(true);
+            btnStop.setEnabled(false);
+        }
+    }
+}
---
+++ app/src/main/java/org/lunapark/dev/intervaltimer/SetTime.java
@@ -0,0 +1,107 @@
+package org.lunapark.dev.intervaltimer;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+
+/**
+ * Created by znak on 13.06.2016.
+ */
+public class SetTime extends DialogFragment {
+
+    // Use this instance of the interface to deliver action events
+    DialogListener mListener;
+
+    public interface DialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog, int time, int interval);
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the DialogListener so we can send events to the host
+            mListener = (DialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        NumberPicker.Formatter formatter = new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                return String.format("%02d", value);
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog, null);
+        final NumberPicker npMinutes = (NumberPicker) view.findViewById(R.id.npMinutes);
+        npMinutes.setMaxValue(59);
+        npMinutes.setMinValue(0);
+        npMinutes.setFormatter(formatter);
+        npMinutes.setWrapSelectorWheel(true);
+        npMinutes.setValue(getArguments().getInt("Min"));
+
+
+        final NumberPicker npSeconds = (NumberPicker) view.findViewById(R.id.npSeconds);
+        npSeconds.setMaxValue(59);
+        npSeconds.setMinValue(0);
+        npSeconds.setFormatter(formatter);
+        npSeconds.setWrapSelectorWheel(true);
+        npSeconds.setValue(getArguments().getInt("Sec"));
+
+
+        final int interval = getArguments().getInt("Interval");
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(view)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the positive button event back to the host activity
+
+                        int time = npMinutes.getValue() * 60 + npSeconds.getValue();
+                        mListener.onDialogPositiveClick(SetTime.this, time, interval);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Send the negative button event back to the host activity
+//                        mListener.onDialogNegativeClick(SetTime.this);
+                    }
+                });
+//                // Add action buttons
+//                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // sign in the user ...
+//                    }
+//                })
+//                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        SetTime.this.getDialog().cancel();
+//                    }
+//                });
+        return builder.create();
+    }
+}
